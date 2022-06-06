f735906dc4aca2c506fcb3af7bf8c9662e0cc194
Alexander <alexanderanufriev111@gmail.com>
2022-06-06 23:11:19 +0300

    resume

--- script.js
+++ script.js
@@ -21,11 +21,24 @@
         })
     });
 
+    let name = "Theme";
+
+    if(document.cookie == 'Theme=dark'){
+        jQuery( "body" ).removeClass( "brightTheme" ).addClass( "darkTheme" );
+    }
+
+    if(document.cookie == 'Theme=bright'){
+        jQuery( "body" ).removeClass( "darkTheme" ).addClass( "brightTheme" );
+    }
 
     jQuery(".buttonDarkTheme" ).click(function() {
+        let value = 'dark';
         jQuery( "body" ).removeClass( "brightTheme" ).addClass( "darkTheme" );
+        document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
     });
 
     jQuery( ".buttonBrightTheme" ).click(function() {
+        let value = 'bright';
         jQuery( "body" ).removeClass( "darkTheme" ).addClass( "brightTheme" );
+        document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
     });
