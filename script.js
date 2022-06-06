3cc7ec66d45b85dfccbd3690aad76f94f828ee99
Alexander <alexanderanufriev111@gmail.com>
2022-06-06 22:21:36 +0300

    resume

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 
 
 
-jQuery('.changeLanguageEN').click(function(e) {
-    $.ajax({
-        type: "POST",
-        url: "en/index.php",
-        success: function(result){
-            $('body').html(result);
-        }
-    })
-});
+    jQuery('.changeLanguageEN').click(function(e) {
+        $.ajax({
+            type: "POST",
+            url: "en/index.php",
+            success: function(result){
+                $('body').html(result);
+            }
+        })
+    });
 
-jQuery('.changeLanguageRU').click(function(e) {
-    $.ajax({
-        type: "POST",
-        url: "/index.php",
-        success: function(result){
-            $('body').html(result);
-        }
-    })
-});
+    jQuery('.changeLanguageRU').click(function(e) {
+        $.ajax({
+            type: "POST",
+            url: "/index.php",
+            success: function(result){
+                $('body').html(result);
+            }
+        })
+    });
+
+
+    $( ".buttonDarkTheme" ).click(function() {
+        $( "body" ).removeClass( "brightTheme" ).addClass( "darkTheme" );
+    });
+
+    jQuery( ".buttonBrightTheme" ).click(function() {
+        $( "body" ).removeClass( "darkTheme" ).addClass( "brightTheme" );
+    });
